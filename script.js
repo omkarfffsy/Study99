76fd65e7614a3158938bb245e1f2988a9146e88c
omkarfffsy <153085569+omkarfffsy@users.noreply.github.com>
2026-05-15 18:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ const seriesData = [
 
 { id: 703, title: "MATHS 1 CH 2 LELELECTUR 1 Matrices", category: "12 CRASH COURSE MATHS 1 2026", isFeatured: false, image: "https://thumbs2.imgbox.com/30/86/dpCnSN6d_t.jpg", episodes: [{ thumbnail: "https://thumbs2.imgbox.com/30/86/dpCnSN6d_t.jpg", mainLink: "https://t.me/File_store_1a21_bot?start=BQADAQADWhAAAlTOaEUGkGl4CPHidhYE" }] },
      
-     
+     { id: 704, title: "MATHS 1 CH 2 LELELECTUR 2 Matrices", category: "12 CRASH COURSE MATHS 1 2026", isFeatured: false, image: "https://thumbs2.imgbox.com/ab/da/uISw0xbX_t.jpg", episodes: [{ thumbnail: "https://thumbs2.imgbox.com/ab/da/uISw0xbX_t.jpg", mainLink: "https://t.me/File_store_1a21_bot?start=BQADAQADKxUAAi5wOEQ-6w_I71UAAZ0WBA" }] },
+
+
      
     
     { id: 301, title: " Solid State", category: "Chemistry", isFeatured: true, image: "https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?w=800&auto=format&fit=crop&q=60", episodes: [{ thumbnail: "https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?w=800&auto=format&fit=crop&q=60", mainLink: "#" }] },
